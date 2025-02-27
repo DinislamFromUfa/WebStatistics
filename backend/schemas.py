8737eb4011e05158f4b39659502f54a4d4f1c7c7
DinislamFromUfa <isberdindinislam@gmail.com>
2025-02-27 15:40:24 +0500

    add patch method of users

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel, Field
+from typing import Optional
+
+from pydantic import BaseModel
 
 
 class UserBase(BaseModel):
@@ -11,4 +13,9 @@ class UserCreate(BaseModel):
     username: str
     email: str
     password: str
+
+class UserUpdate(BaseModel):
+    username: Optional[str] = None
+    email: Optional[str] = None
+    password: Optional[str] = None
 
